861d32d2c338e0d8d10f615111441d5a55c88f1a
Apocalipsis679 <apocalipsis679@gmail.com>
2022-10-09 21:37:35 +0300

    добавлен .trim

--- js/script.js
+++ js/script.js
@@ -27,9 +27,9 @@ let numberOfFilms;
 
 
 function start () {
-  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "" );
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "" ).trim();
   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "").trim();
   }
 }
 // start();
@@ -51,8 +51,8 @@ let personalMovieDB = {
 
 function rememberMyFilms() {
   for (let i = 0; i < 2; i++) {
-    const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", "");
+    const a = prompt("Один из последних просмотренных фильмов?", "").trim(),
+      b = prompt("На сколько оцените его?", "").trim();
   
     if (a != null && b != null && a != "" && b != "" && a.length < 50) {
       personalMovieDB.movies[a] = b;
@@ -95,7 +95,7 @@ showMyDB(personalMovieDB.privat);
 
 function writeYourGenres() {
 for (let i = 1; i <= 3; i++) {
-   personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+   personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`).trim();
 }
 }
 writeYourGenres();
